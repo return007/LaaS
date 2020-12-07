ed1c3b967dfe7e409c351bf5e26a7a3d8c51a93e
Gaurav Lalchandani <glalchandanig@gmail.com>
2020-12-08 01:19:52 +0530

    Add some more features to the overall atlaas infra
     1. Users can now create their configuration by filling in data
        in a form.
    
     2. The form submit generates a config file, which needs to be
        downloaded by the user. This config file needs one modification
        before it can be used to generate leaderboard.
    
     3. Post download steps are also mentioned on the same website.

--- atlaas/bin/start.py
+++ atlaas/bin/start.py
@@ -10,7 +10,7 @@ This fellow is a WSGI application, responsible for:
 
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from os import path
 
 
@@ -27,9 +27,46 @@ app = Flask(
 )
 
 
+def build_configuration(contents):
+    return {
+        "title": contents.get('event_name', 'Event Name - 2020'),
+        "tabs": [
+            {
+                "title": contents.get('leaderboard_title', 'Leaderboard'),
+                "type": contents.get('leaderboard_type', 'Ranking').lower(),
+                "highlight_winners": True,
+                "winners_count": 3,
+                "data": "/PATH/TO/YOUR/RANKINGS/CSV/FILE",
+                "event_status": "live",
+                "contest_link": contents.get('event_link',
+                                             'https://event.com/example')
+            }
+        ]
+    }
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/create')
+def get_started():
+    return render_template('step_1.html')
+
+
+@app.route('/finish', methods=['POST'])
+def form_submit():
+    # Extract all the information from the form
+    # Create config file
+    # Ask user to download
+    # Also instruct user to install and run laas script
+    post_data = request.form
+    print(post_data)
+    return render_template(
+        'step_2.html',
+        configuration=build_configuration(post_data)
+    )
 
 
 if __name__ == '__main__':
